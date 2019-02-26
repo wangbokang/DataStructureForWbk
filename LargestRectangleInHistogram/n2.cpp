f82707a151ac13d1cde9f354c022da9d48990b90
wangbokang <48026815+wangbokang@users.noreply.github.com>
2019-02-27 01:02:44 +0800

    already worked

--- LargestRectangleInHistogram/n2.cpp
+++ LargestRectangleInHistogram/n2.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) 
+    {
+        int n = heights.size();
+        int result = 0;
+        int j1 , j2;
+        for(int i = 0; i < n; i++)
+        {
+            for(j1 = i-1; j1 >= 0; j1--)
+            {
+                if(heights[i] > heights[j1])
+                    break;
+            }
+            for(j2 = i+1; j2 <= n-1; j2++)
+            {
+                if(heights[i] > heights[j2])
+                    break;
+            }
+            result = max(result , (j2 - j1-1 ) * heights[i]);
+        }
+        return result;
+    }
+};
